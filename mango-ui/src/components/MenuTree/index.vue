2e7ad1530967826866402e6e374921eae67fac7f
you-know-who-2017 <1376397133@qq.com>
2021-01-01 13:35:30 +0800

    update Menutree

---
+++ mango-ui/src/components/MenuTree/index.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Author: geekli
+ * @Date: 2021-01-01 13:35:13
+ * @LastEditTime: 2021-01-01 13:35:14
+ * @LastEditors: your name
+ * @Description:
+ * @FilePath: /mango-ui/src/components/MenuTree/index.vue
+-->
+<template>
+  <el-submenu v-if="menu.children && menu.children.length >= 1" :index="'' + menu.id">
+    <template slot="title">
+      <i :class="menu.icon" ></i>
+      <span slot="title">{{menu.name}}</span>
+    </template>
+    <MenuTree v-for="item in menu.children" :key="item.id" :menu="item"></MenuTree>
+  </el-submenu>
+  <el-menu-item v-else :index="'' + menu.id" @click="handleRoute(menu)">
+    <i :class="menu.icon"></i>
+    <span slot="title">{{menu.name}}</span>
+  </el-menu-item>
+</template>
+
+<script>
+import { getIFrameUrl, getIFramePath } from '@/utils/iframe'
+export default {
+  name: 'MenuTree',
+  props: {
+    menu: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    handleRoute (menu) {
+      // 通过菜单URL跳转至指定路由
+      this.$router.push("/" + menu.url)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
